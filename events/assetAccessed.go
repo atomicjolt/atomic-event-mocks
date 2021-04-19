28e4ca3f876abeb5e5279b3e31eefe833f4a592e
Carson Fox <carson.fox@atomicjolt.com>
2021-04-19 11:54:25 -0400

    Asset accessed

---
+++ events/assetAccessed.go
@@ -0,0 +1,46 @@
+package events
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+)
+
+type AssetAccessed struct {
+	Metadata `json:"metadata" fake:"skip"`
+	Body     struct {
+		AssetName    string `json:"asset_name" fake:"{buzzword}"`
+		AssetType    string `json:"asset_type" fake:"skip"`
+		AssetId      string `json:"asset_id" fake:"{number:1,10}"`
+		AssetSubtype string `json:"asset_subtype" fake:"{buzzword}"`
+		Category     string `json:"category" fake:"{buzzword}"`
+		Role         string `json:"role" fake:"{buzzword}"`
+		Level        string `json:"level" fake:"{buzzword}"`
+	} `json:"body"`
+}
+
+func MockAssetAccessed() AssetAccessed {
+	assetTypes := []string{
+		"group",
+		"course",
+		"enrollment",
+		"user",
+		"collaboration",
+		"account",
+		"quizzes:quiz",
+		"lti/tool_proxy",
+		"assignment",
+		"calendar_event",
+		"web_conference",
+		"learning_outcome",
+		"wiki_page",
+		"discussion_topic",
+		"attachment",
+	}
+
+	var mock AssetAccessed
+	mock.Metadata = mockMetaData("asset_accessed")
+	gofakeit.Struct(&mock.Body)
+
+	mock.Body.AssetType = gofakeit.RandomString(assetTypes)
+
+	return mock
+}
