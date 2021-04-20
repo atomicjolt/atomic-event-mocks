f4c821e74ffb7af06a472644eb08c654cfcdf333
Carson Fox <carson.fox@atomicjolt.com>
2021-04-20 12:18:26 -0400

    Better use of embedding

--- events/assignmentUpdated.go
+++ events/assignmentUpdated.go
@@ -1,34 +1,11 @@
 package events
 
-import (
-	"github.com/brianvoe/gofakeit/v6"
-)
-
 type AssignmentUpdated struct {
 	AssignmentCreated
 }
 
 func MockAssignmentUpdated() AssignmentUpdated {
 	var mock AssignmentUpdated
-	mock.Metadata = mockMetaData("assignment_updated")
-	gofakeit.Struct(&mock.Body)
-
-	mock.Body.ContextType = "Course"
-	mock.Body.WorkflowState = "published"
-
-	submissionTypes := []string{
-		"discussion_topic",
-		"external_tool",
-		"media_recording",
-		"none",
-		"not_graded",
-		"online_quiz",
-		"online_text_entry",
-		"online_upload",
-		"online_url",
-		"on_paper",
-	}
-	mock.Body.SubmissionTypes = gofakeit.RandomString(submissionTypes)
-
+	mock.AssignmentCreated = MockAssignmentCreated()
 	return mock
 }
--- events/moduleItemUpdated.go
+++ events/moduleItemUpdated.go
@@ -1,21 +1,11 @@
 package events
 
-import (
-	"github.com/brianvoe/gofakeit/v6"
-)
-
 type ModuleItemUpdated struct {
-	//Has the same payload as the created event
 	ModuleItemCreated
 }
 
 func MockModuleItemUpdated() ModuleItemUpdated {
 	var mock ModuleItemUpdated
-	mock.Metadata = mockMetaData("module_item_updated")
-	gofakeit.Struct(&mock.Body)
-
-	mock.Body.ContextType = "Course"
-	mock.Body.WorkflowState = "active"
-
+	mock.ModuleItemCreated = MockModuleItemCreated()
 	return mock
 }
