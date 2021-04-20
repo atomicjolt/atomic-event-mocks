e12c9a18e24a6e3471aa98dac097a31fa16ed76d
Carson Fox <carson.fox@atomicjolt.com>
2021-04-20 14:41:21 -0400

    Group created

---
+++ events/groupCreated.go
@@ -0,0 +1,28 @@
+package events
+
+type GroupCreated struct {
+	Metadata `json:"metadata"`
+
+	Body struct {
+		AccountId         string `json:"account_id" fake:"{number:1,100}"`
+		ContextId         string `json:"context_id" fake:"{number:1,100}"`
+		ContextType       string `json:"context_type" fake:"skip"`
+		GroupCategoryId   string `json:"group_category_id" fake:"{number:1,100}"`
+		GroupCategoryName string `json:"group_category_name" fake:"{buzzword}"`
+		GroupId           string `json:"group_id" fake:"{number:1,100}"`
+		GroupName         string `json:"group_name" fake:"{buzzword}"`
+		MaxMembership     int    `json:"max_membership" fake:"{number:1,100}"`
+		Uuid              string `json:"uuid" fake:"{uuid}"`
+		WorkflowState     string `json:"workflow_state" fake:"skip"`
+	} `json:"body"`
+}
+
+func MockGroupCreated() GroupCreated {
+	var mock GroupCreated
+	mock.Metadata = mockMetaData("group_created")
+
+	mock.Body.ContextType = "Course"
+	mock.Body.WorkflowState = "active"
+
+	return mock
+}
